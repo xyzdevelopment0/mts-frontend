7618c109e9305bd89e153cd6879a35b55c2bc8a0
xyzdevelopment0 <xyzdevelopment@icloud.com>
2026-03-04 10:09:39 +0300

    feat: add visitors.now tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import '@/assets/globals.css'
 
 export const metadata: Metadata = {
@@ -13,6 +14,12 @@ type LayoutProps = Readonly<{
 export default function RootLayout({ children }: LayoutProps) {
 	return (
 		<html lang='en'>
+			<head>
+				<Script
+					src='https://cdn.visitors.now/v.js'
+					data-token='0128e893-cfef-4a94-b41b-1aae801b8438'
+				/>
+			</head>
 			<body>{children}</body>
 		</html>
 	)
